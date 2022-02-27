765b292d68aa4af412bb95ec4e4c61f3ae664d0b
Merten Popp <webmaster@merten-popp.de>
2022-02-27 04:52:46 +0100

    compile with clang

--- Toolchain-i686.cmake
+++ Toolchain-i686.cmake
@@ -2,8 +2,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
 # Which compilers to use for C and C++
-set(CMAKE_C_COMPILER gcc -m32)
-set(CMAKE_CXX_COMPILER g++ -m32)
+set(CMAKE_C_COMPILER clang -m32)
+set(CMAKE_CXX_COMPILER clang++ -m32)
 
 #https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_LIBRARY_ARCHITECTURE.html#variable:CMAKE_%3CLANG%3E_LIBRARY_ARCHITECTURE
 set(CMAKE_LIBRARY_ARCHITECTURE i686)
