09a9f24118fddc97c8f43a5192a0cac0a39ce29a
wackfx <hello@wackfx.com>
2023-12-31 00:10:50 +0100

    fix: Avoid writing options everywhere fix: Reorganized Takeoff.cpp to allow for one-shot CLI run feat: Allow for CLI one-shot run without creating any AT.json artifact

--- src/Sim.cpp
+++ src/Sim.cpp
@@ -43,7 +43,7 @@ static long InitMoney[] = {1500000, 0,        2000000, 0,                       
 static SLONG MonthLength[] = {31, 28, 31, 30, 31, 30, 30, 31, 30, 31, 30, 31};
 
 char chRegKey[] = R"(Software\Spellbound Software\Airline Tycoon Deluxe\1.0)";
-char chRegKeyOld[] = R"(Software\Spellbound Software\Airline Tycoon Evolution\1.0)";
+// char chRegKeyOld[] = R"(Software\Spellbound Software\Airline Tycoon Evolution\1.0)";
 // char chRegKeyOld[] = "Software\\Spellbound Software\\Airline Tycoon FirstClass\\1.0";
 
 extern SLONG NewgameWantsToLoad;
@@ -142,7 +142,7 @@ SIM::SIM() {
 //--------------------------------------------------------------------------------------------
 // Destruktor:
 //--------------------------------------------------------------------------------------------
-SIM::~SIM() { SaveOptions(); }
+SIM::~SIM() {}
 
 //--------------------------------------------------------------------------------------------
 // Fügt einen Smacker im Flughafen hinzu:
@@ -4380,8 +4380,6 @@ void COptions::ReadOptions() {
             Sim.GameSpeed = 30;
         }
     }
-
-    WriteOptions();
 }
 
 //--------------------------------------------------------------------------------------------
@@ -4498,6 +4496,7 @@ dont_save_talking:
     Sim.MaxDifficulty = tmp;
     Sim.MaxDifficulty2 = tmp2;
     Sim.MaxDifficulty3 = tmp3;
+    reg.WriteFile();
 }
 
 //--------------------------------------------------------------------------------------------
